a7490f5bb058862ae98b81aa209138679099fb50
joshmt <>
2010-02-26 12:50:39 +0000

    more robust version

--- CUJEMtools/mergeQCD.C
+++ CUJEMtools/mergeQCD.C
@@ -11,52 +11,72 @@ using namespace std;
 
 void mergeQCD( TString cutstring="CutsJMTNoJetVeto" )
 {
+  //input path
+  const TString path="CUJEM_r1.4/"; //adjust this as needed
 
-  float weights[] = {66.0342,3.703,0.1292,0.005181}; //these are weights for 100 pb^-1
+  vector<float> weights;
+  vector<TString> qcd;
 
-  float total=0;
-  for (int i=0; i<4;i++)     total+=weights[i];
-
-  TString path="CUJEM_r1.3/";
-
-  TString qcd[]={"","","",""};
-  qcd[0] = "plots_QCD_Pt100to250-madgraph_";
-  qcd[1] = "plots_QCD_Pt250to500-madgraph_";
-  qcd[2] = "plots_QCD_Pt500to1000-madgraph_";
-  qcd[3] = "plots_QCD_Pt1000toInf-madgraph_";
-  
-  TFile* fqcd[4];
-
-  for (int i=0;i<4;i++) {
-    qcd[i] += cutstring;
-    qcd[i] += ".root";
-    qcd[i].Prepend(path);
-    fqcd[i] = new TFile(qcd[i]);
+  const TString kindofsample="pythia";
+  if (kindofsample=="madgraph") {
+    cout<<"Using madgraph samples!"<<endl;
+    //weights -- adjust as needed
+    weights.push_back(66.0342); //these are weights for 100 pb^-1
+    weights.push_back(3.703);
+    weights.push_back(0.1292);
+    weights.push_back(0.005181);  
+    
+    //file names -- adjust as needed
+    qcd.push_back( "plots_QCD_Pt100to250-madgraph_");
+    qcd.push_back( "plots_QCD_Pt250to500-madgraph_");
+    qcd.push_back("plots_QCD_Pt500to1000-madgraph_");
+    qcd.push_back("plots_QCD_Pt1000toInf-madgraph_");
   }
+  else if (kindofsample=="pythia") {
+    cout<<"Using pyhtia samples!"<<endl;
 
+    weights.push_back(28.8377); //these are weights for 100 pb^-1
+    weights.push_back(0.813167135);
+
+    qcd.push_back("plots_QCD_Pt80_");
+    qcd.push_back("plots_QCD_Pt170_");
+
+  }
+  //  float total=0;
+  //  for (unsigned int i=0; i<weights.size();i++)     total+=weights.at(i);
+
+  vector<TFile*> fqcd;
+  for (unsigned int i=0;i<weights.size();i++) {
+    TString filename=qcd.at(i);
+    filename+= cutstring;
+    filename+=".root";
+    filename.Prepend(path);
+    TFile* ftmp=new TFile(filename);
+    if (ftmp->IsZombie()) cout<<"Problem loading file with name="<<filename<<endl;
+    fqcd.push_back( ftmp);
+  }
   TString outname="plots_QCD_";
   outname+=cutstring;
   outname+=".root";
   TFile fout(outname,"RECREATE");
 
   //we've got all of the files open, so let's loop over the keys in a file and add them up!
-  //TList* keys= fqcd[0]->GetListOfKeys();
 
   TIter nextkey(fqcd[0]->GetListOfKeys());
   TKey *key;
 
   while (key = (TKey*) nextkey() ) {
-    cout<<"-- new key --"<<endl;
+    //    cout<<"-- new key --"<<endl;
     TH1F* th1=0;
     TH2F* th2=0;
     TH1F* h1=0;
     TH2F* h2=0;
     if ( key->ReadObj()->InheritsFrom("TH2F") ) {
-      cout<<"2D: Key="<<key->ReadObj()->GetName()<<endl;
+      //      cout<<"2D: Key="<<key->ReadObj()->GetName()<<endl;
       th2=(TH2F*) key->ReadObj();
     }
     else if ( key->ReadObj()->InheritsFrom("TH1F") ) {
-      cout<<"1D: Key="<<key->ReadObj()->GetName()<<endl;
+      //      cout<<"1D: Key="<<key->ReadObj()->GetName()<<endl;
       th1=(TH1F*) key->ReadObj();
     }
     else {
@@ -65,32 +85,30 @@ void mergeQCD( TString cutstring="CutsJMTNoJetVeto" )
     }
 
     fout.cd();
-    cout<<"-- about to clone --"<<endl;
+    //    cout<<"-- about to clone --"<<endl;
     if (th1!=0) { 
       h1=(TH1F*) th1->Clone();
       h1->Sumw2();
-      h1->Scale(weights[0]);
+      h1->Scale(weights.at(0));
     }
     else if (th2!=0) {
       h2=(TH2F*) th2->Clone();
       h2->Sumw2();
-      h2->Scale(weights[0]);
+      h2->Scale(weights.at(0));
     }
 
-    cout<<" -- about to add subsequent histos --"<<endl;
-    for (int i=1; i<4; i++) {
-
-      //this seems to work! we need to add the weights in here now!!
+    //    cout<<" -- about to add subsequent histos --"<<endl;
+    for (unsigned int i=1; i<fqcd.size(); i++) {
 
       if (h1!=0) {
-	th1 = (TH1F*) fqcd[i]->Get(th1->GetName());
+	th1 = (TH1F*) fqcd.at(i)->Get(th1->GetName());
 	//cout<<th1<<endl;
-	h1->Add(th1,weights[i]);
+	h1->Add(th1,weights.at(i));
       }
       else if (h2!=0) {
-	th2 = (TH2F*) fqcd[i]->Get(th2->GetName());
+	th2 = (TH2F*) fqcd.at(i)->Get(th2->GetName());
 	//cout<<th2<<endl;
-	h2->Add(th2,weights[i]);
+	h2->Add(th2,weights.at(i));
       }
 
     }
@@ -100,8 +118,7 @@ void mergeQCD( TString cutstring="CutsJMTNoJetVeto" )
     else if (h2!=0) h2->Write();
   }
 
-  //    TH2F* h2=dynamic_cast<TH2F*> keys->At(i);
-  cout<<"Finished write file "<<outname<<endl;
+  cout<<"Finished writing file "<<outname<<endl;
   fout.Close();
 
 }
