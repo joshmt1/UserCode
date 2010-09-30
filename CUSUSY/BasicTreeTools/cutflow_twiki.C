fd76d3e54ae5bf68915981b6a6ea38539d56df3a
joshmt <>
2010-09-30 09:15:26 +0000

    add plotting

--- CUSUSY/BasicTreeTools/cutflow_twiki.C
+++ CUSUSY/BasicTreeTools/cutflow_twiki.C
@@ -9,19 +9,30 @@
 usage:
 root -b -l -q cutflow_twiki.C++
 */
+//#include "TROOT.h"
+#include "TStyle.h"
+#include "TCanvas.h"
+#include "TH1D.h"
+#include "THStack.h"
+#include "TLegend.h"
+#include "TAxis.h"
+TCanvas * Ccutflow;
 
-//#include "TCanvas.h"
-//TCanvas * Ccutflow;
 
 void cutflow_twiki()
 {
+  //  gROOT->SetStyle("BABAR");
   //first we need to load each text file (one sample at a time)
-  const TString filestub ="cutflow_RA2tcMET";
+  const TString filestub ="cutflow_RA2METminDP";
 
-  const int mode = 1; //mode 1 is print cut flow table ; mode 2 is print S/sqrt(S+B) table ; mode 3 is S/sqrt(B)
+  const int mode = 3; //mode 1 is print cut flow table ; mode 2 is print S/sqrt(S+B) table ; mode 3 is S/sqrt(B)
   assert(mode==1 || mode==2 || mode ==3);
 
-  //  const bool drawplots=true;
+  TString modeDescription="";
+  if (mode==2) modeDescription="S/sqrt(S+B)";
+  else if (mode==3) modeDescription="S/sqrt(B)";
+
+  int icolor=1;
 
   //in principle these are coded in basicLoop.C/h; do the easy thing for now
   std::vector<TString> cutnames;
@@ -29,12 +40,12 @@ void cutflow_twiki()
   cutnames.push_back("Trigger");
   cutnames.push_back("PV");
   cutnames.push_back(">= 3 Jets");
-  cutnames.push_back("HT Cut");
-  if (filestub.Contains("MET")) { cutnames.push_back("MET Cut")}
-  else { cutnames.push_back("MHT Cut");}
-  cutnames.push_back("Muon veto");
-  cutnames.push_back("electron veto");
-  cutnames.push_back("Delta Phi");
+  cutnames.push_back("HT");
+  if (filestub.Contains("MET")) { cutnames.push_back("MET");}
+  else { cutnames.push_back("MHT");}
+  cutnames.push_back("#mu veto");
+  cutnames.push_back("e veto");
+  cutnames.push_back("#Delta #phi");
   cutnames.push_back(">=1 b");
   cutnames.push_back(">=2 b");
   cutnames.push_back(">=3 b");
@@ -49,6 +60,10 @@ void cutflow_twiki()
   //  char *signal_list[]={"LM0", "LM1", "LM2", "LM4", "LM5", "LM6","LM7", "LM8","LM9","LM9p", "LM9t175", "LM10", "LM11", "LM12","LM13","mMSSM"};
   char *signal_list[]={"LM9","mMSSM","mMSSMv2","mMSSMv3"};
 
+  //these are only relevant for mode==1
+  const int drawsignalindex = 3; //this is the signal to use for the plot
+  const TString mysig = signal_list[drawsignalindex];
+
   //as long as i compile, I can use the most basic stl containers
   // ... what was I thinking? what i really want is a map of these, indexed by the names.
   //can ROOT handle it? //seems ok....
@@ -58,6 +73,9 @@ void cutflow_twiki()
 
   std::map<TString, std::vector<float> > background;
   std::map<TString, std::vector<float> > backgrounderr;
+  //probably I could have used a TH1 as a storage container from the beginning
+  //but keep the old STL structure in order to avoid bugs
+  std::map<TString, TH1D*> Hcutflow;
 
   std::map<TString, std::vector<float> > signal;
   std::map<TString, std::vector<float> > signalerr;
@@ -79,18 +97,47 @@ void cutflow_twiki()
     file.close();
   }
 
+  if (mode==1) {
+    Hcutflow[TString("QCD")] = new TH1D("HQCD","QCD",cutnames.size(),0,cutnames.size());
+    Hcutflow["QCD"]->SetLineColor(icolor);   Hcutflow["QCD"]->SetMarkerColor(icolor);
+    Hcutflow["QCD"]->SetFillColor(icolor);
+    icolor++;
+  }
+
+  if (mode==1) {
+    Hcutflow[mysig] = new TH1D("Hsignal",mysig,cutnames.size(),0,cutnames.size());
+    Hcutflow[mysig]->SetLineColor(icolor);   Hcutflow[mysig]->SetMarkerColor(icolor);
+    Hcutflow[mysig]->SetFillColor(icolor);
+    icolor++;
+  }
+
   for (int i=0 ; i<nbackground; i++) {
     TString filename = filestub;
     filename+="."; filename+=background_list[i];
     filename += ".dat";
 
+    if (mode==1) {
+      Hcutflow[TString(background_list[i])] = new TH1D("H"+TString(background_list[i]),TString(background_list[i]),cutnames.size(),0,cutnames.size());
+      cout<<"Created histogram name = "<<Hcutflow[TString(background_list[i])]->GetName()<<endl;
+      Hcutflow[TString(background_list[i])]->SetLineColor(icolor); 
+      Hcutflow[TString(background_list[i])]->SetMarkerColor(icolor);
+      Hcutflow[TString(background_list[i])]->SetFillColor(icolor);
+      icolor++;
+    }
+    
     cout<<"Reading "<<filename<<endl;
     float nevt,nevterr;
     ifstream file(filename.Data());
     if (!file.good()) {cout<<"bad file! "<<filename<<endl; return;}
+    int ibin=1;
     while (file>>nevt>>nevterr ) {
       background[TString(background_list[i])].push_back(nevt);
       backgrounderr[TString(background_list[i])].push_back(nevterr);
+      if (mode==1) {
+	Hcutflow[TString(background_list[i])]->SetBinContent(ibin,nevt);
+	Hcutflow[TString(background_list[i])]->SetBinError(ibin,nevterr);
+	ibin++;
+      }
     }
     file.close();
     //    cout<<  background[TString(background_list[i])].size()<<"\t"<<backgrounderr[TString(background_list[i])].size()<<endl;
@@ -103,6 +150,15 @@ void cutflow_twiki()
     filename+="."; filename+=signal_list[i];
     filename += ".dat";
 
+    if (mode==2 ||mode ==3) {
+      Hcutflow[TString(signal_list[i])] = new TH1D("H"+TString(signal_list[i]),TString(signal_list[i]),cutnames.size(),0,cutnames.size());
+      cout<<"Created histogram name = "<<Hcutflow[TString(signal_list[i])]->GetName()<<endl;
+      Hcutflow[TString(signal_list[i])]->SetLineColor(icolor); 
+      Hcutflow[TString(signal_list[i])]->SetMarkerColor(icolor);
+      //      Hcutflow[TString(signal_list[i])]->SetFillColor(icolor); //don't want this for modes 2 and 3
+      icolor++;
+    }
+    
     cout<<"Reading "<<filename<<endl;
     float nevt,nevterr;
     ifstream file(filename.Data());
@@ -133,6 +189,7 @@ void cutflow_twiki()
   for (int isignal=0 ; isignal<nsignal; isignal++)     cout<<signal_list[isignal]<<col;
   cout<<endl;
 
+
   for (int i=0; i<ncuts; i++) {
     cout<<col<< i<<col<<cutnames.at(i)<<col;
 
@@ -146,6 +203,11 @@ void cutflow_twiki()
       qcd_total += qcd[qcd_list[iqcd]].at(i);
       qcd_total_err += pow(qcderr[qcd_list[iqcd]].at(i),2);
     }
+    if (mode ==1) {
+      Hcutflow["QCD"]->SetBinContent(i+1,qcd_total);
+      Hcutflow["QCD"]->SetBinError(i+1,sqrt(qcd_total_err));
+    }
+
     float background_total=qcd_total;
     float background_total_err=qcd_total_err; //before taking the square root
 
@@ -174,6 +236,21 @@ void cutflow_twiki()
 	  cout<<signal[signal_list[isignal]].at(i)/sqrt(background_total)  << col;
 	else  cout<<" - "<<endl;
       }
+      if (mode ==1 && isignal == drawsignalindex) {
+	  Hcutflow[mysig]->SetBinContent(i+1,signal[signal_list[isignal]].at(i));
+	  Hcutflow[mysig]->SetBinError(i+1,signalerr[signal_list[isignal]].at(i));
+      }
+      else if (mode==2) {
+	if (signal[signal_list[isignal]].at(i)+ background_total >0) {
+	  //not so keen on the fact that I'm duplicating the calculation code here...
+	  Hcutflow[signal_list[isignal]]->SetBinContent(i+1, signal[signal_list[isignal]].at(i)/sqrt(signal[signal_list[isignal]].at(i)+ background_total));
+	}
+      }
+      else if (mode==3) {
+	if ( background_total >0) 
+	  Hcutflow[signal_list[isignal]]->SetBinContent(i+1,signal[signal_list[isignal]].at(i)/sqrt(background_total) );
+      }
+      
     }
     
     cout<<endl;
@@ -183,6 +260,58 @@ void cutflow_twiki()
   //add signal;  --done
   //compute s/root(s+b)
   //etc
+
+  Ccutflow = new TCanvas("Ccutflow");
+  if (mode==1)  Ccutflow->SetLogy();
+  THStack mystack("mystack","Cut Flow Steps"); //only used for mode 1
+  float legx1=0.6,legy1=0.6,legx2=0.9,legy2=0.9;
+  if (mode==2 || mode==3) {
+    legx1=0.2; legx2=0.5;
+  }
+  TLegend leg(legx1,legy1,legx2,legy2);
+  leg.SetFillColor(0);
+
+  TString opt="";
+  double highest=0;
+  for ( std::map<TString, TH1D*>::const_iterator ib=Hcutflow.begin(); ib!=Hcutflow.end(); ++ib) {
+    if (mode==1) mystack.Add( ib->second );
+    else {
+      ib->second->Draw(opt);
+      opt="SAME";
+      double height=ib->second->GetMaximum();
+      if (height>highest) highest=height;
+    }
+    leg.AddEntry(ib->second,ib->first);
+  }
+
+  gStyle->SetOptStat(0);
+  TAxis* ax=0;
+  if (mode==1) {
+    mystack.Draw("HIST");
+    TH1* hax=mystack.GetHistogram();
+    cout<<hax<<endl;
+    ax = hax->GetXaxis();
+  }
+  else { //this is tricky
+    ax= Hcutflow.begin()->second->GetXaxis();
+    Hcutflow.begin()->second->SetMaximum(highest*1.05);
+    Hcutflow.begin()->second->SetTitle("");
+    Hcutflow.begin()->second->SetYTitle(modeDescription);
+  }
+  cout<<ax<<endl;
+  
+  for (unsigned int icut=0; icut<cutnames.size(); icut++) {
+    ax->SetBinLabel(icut+1,cutnames.at(icut));
+  }
+  leg.Draw();
+
+  TString outname = filestub;
+  outname+=".";
+  outname+=mysig;
+  outname+=".";
+  outname+=mode;
+  outname+=".eps";
+  Ccutflow->SaveAs(outname);
 
   /*
 
