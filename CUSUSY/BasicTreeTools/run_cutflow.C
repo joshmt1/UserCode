58342718b96240a4f4c31cd4571fda796c685a0a
joshmt <>
2010-10-05 16:10:54 +0000

    minor

--- CUSUSY/BasicTreeTools/run_cutflow.C
+++ CUSUSY/BasicTreeTools/run_cutflow.C
@@ -46,49 +46,15 @@ void run_cutflow()
 
     cout<<"About to start on files: "<<samplefiles<<endl;
 
-    //if (!samplefiles.Contains("v2")) continue; //hack to skip some samples
-
+    if (samplefiles.Contains("LM") || samplefiles.Contains("MSSM")) continue; //hack to skip some samples
+    
     TChain ch("BasicTreeMaker/tree");
     ch.Add(samplefiles);
     basicLoop looper(&ch);
-    looper.setCutScheme(basicLoop::kRA2tcMET); //this is a critical line! defines the cuts to use!
+    looper.setCutScheme(basicLoop::kRA2METminDP); //this is a critical line! defines the cuts to use!
     looper.setBCut(3); //require 3 b tags so that we make the full cut flow table
     looper.cutflow();
   }
 
-  //the old fashioned way
-
-//   {
-//     //QCD-Pt100to250-madgraph
-//     TChain* qcd100Chain = new TChain("BasicTreeMaker/tree");
-//     qcd100Chain->Add("/cu1/joshmt/BasicNtuples/V00-00-01/QCD-Pt100to250-madgraph/Spring10/BasicNtuple*");
-    
-//     basicLoop l_qcd100(qcd100Chain);
-//     l_qcd100.cutflow();
-//   }
-
-//   {
-//   //QCD-Pt250to500-madgraph
-//   TChain* qcd250Chain = new TChain("BasicTreeMaker/tree");
-//   qcd250Chain->Add("/cu1/joshmt/BasicNtuples/V00-00-01/QCD-Pt250to500-madgraph/Spring10/BasicNtuple*");
-
-//   basicLoop l_qcd250(qcd250Chain);
-//   l_qcd250.cutflow();
-//   }
-  
-
-//   //QCD-Pt500to1000-madgraph
-//   TChain* qcd500Chain = new TChain("BasicTreeMaker/tree");
-//   qcd500Chain->Add("/cu1/joshmt/BasicNtuples/V00-00-01/QCD-Pt500to1000-madgraph/Spring10/BasicNtuple*");
-
-//   basicLoop l_qcd500(qcd500Chain);
-//   l_qcd500.cutflow();
-  
-//   //QCD-Pt1000toInf-madgraph
-//   TChain* qcd1000Chain = new TChain("BasicTreeMaker/tree");
-//   qcd1000Chain->Add("/cu1/joshmt/BasicNtuples/V00-00-01/QCD-Pt1000toInf-madgraph/Spring10/BasicNtuple*");
-
-//   basicLoop l_qcd1000(qcd1000Chain);
-//   l_qcd1000.cutflow();
 
 }
