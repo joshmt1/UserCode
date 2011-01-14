7eaae386b6ded4708a849b42731bdc0874700d0f
joshmt <>
2011-01-14 10:13:06 +0000

    This commit was manufactured by cvs2svn to create tag 'jmtFinalSpring10'.
    Sprout from master 2011-01-14 10:13:05 UTC joshmt 'keep cvs in sync'
    Delete:
        CUJEMtools/eventLoop.C
        CUJEMtools/mergeQCD.C
        CUSUSY/BasicTreeMaker/BuildFile
        CUSUSY/BasicTreeMaker/README
        CUSUSY/BasicTreeMaker/interface/BasicTreeMaker.h
        CUSUSY/BasicTreeMaker/python/basictreemaker_cfi.py
        CUSUSY/BasicTreeMaker/src/BasicTreeMaker.cc
        CUSUSY/BasicTreeMaker/test/basictreemaker_cfg.py
        CUSUSY/BasicTreeMaker/test/switchCastorDir.sh
        CUSUSY/FindEventNumber/BuildFile
        CUSUSY/FindEventNumber/src/FindEventNumber.cc
        CUSUSY/SupplementaryNtuple/BuildFile
        CUSUSY/SupplementaryNtuple/interface/SupplementaryNtuple.h
        CUSUSY/SupplementaryNtuple/python/SupplementaryNtuple_cfi.py
        CUSUSY/SupplementaryNtuple/src/SupplementaryNtuple.cc
        HistHolder.cxx
        HistHolder.h
        MiscUtil.cxx
        PlotUtil.cxx
        PlotUtil.h
        TopTupleTools/drawFromFlatTuples.C
        TopTupleTools/eventLoop.C
        TopTupleTools/eventLoop.h
        onlyLast.py

--- CUSUSY/SupplementaryNtuple/python/SupplementaryNtuple_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-SupplementaryNtuple = cms.EDAnalyzer('SupplementaryNtuple',
-                                     genParticles = cms.InputTag("genParticles")
-                                     )
