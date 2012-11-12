c20c4b216c4b758fddaaf423558b27e619156ad9
joshmt <>
2012-11-12 23:03:00 +0000

    big update to makeTauTree

--- PatTools/MakeTauTree/src/MakeTauTree.cc
+++ PatTools/MakeTauTree/src/MakeTauTree.cc
@@ -10,13 +10,15 @@
 
 Goal is to study the decays of ttbar, in particular the tau rejection possibilities
 
+Update -- I will now expand this code to try to put all sorts of useful quantities into the tree...
+
  Implementation:
      [Notes on implementation]
 */
 //
 // Original Author:  Joshua Thompson,6 R-029,+41227678914,
 //         Created:  Wed Jul 25 15:22:44 CEST 2012
-// $Id: MakeTauTree.cc,v 1.2 2012/08/21 09:48:00 joshmt Exp $
+// $Id: MakeTauTree.cc,v 1.3 2012/08/27 14:06:50 joshmt Exp $
 //
 //
 
@@ -40,7 +42,8 @@ Goal is to study the decays of ttbar, in particular the tau rejection possibilit
 #include "DataFormats/Candidate/interface/Candidate.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 
-#include "DataFormats/PatCandidates/interface/MET.h"
+#include "DataFormats/METReco/interface/MET.h"
+   //#include "DataFormats/PatCandidates/interface/MET.h"
 #include "DataFormats/PatCandidates/interface/Jet.h"
 #include "DataFormats/PatCandidates/interface/Tau.h"
 #include "DataFormats/MuonReco/interface/Muon.h"
@@ -48,8 +51,8 @@ Goal is to study the decays of ttbar, in particular the tau rejection possibilit
 #include "DataFormats/Math/interface/deltaR.h"
 
 //tau veto is precomputed but we want access to some of the pieces
-#include "SandBox/Skims/src/IndirectTauVeto.cc"
-
+//#include "SandBox/Skims/src/IndirectTauVeto.cc"
+#include "StevenCode/LeptonSelection/src/IndirectTau.cc" //new location within steven's code
 
 //ROOT includes
 #include "TTree.h"
@@ -83,6 +86,7 @@ class MakeTauTree : public edm::EDAnalyzer {
   int getTopDecayCategory(int code1, int code2);
 
   void isolatedTrackVeto(const edm::Event& iEvent, const edm::EventSetup& iSetup);
+  void isolatedTrackDetails(const edm::Event& iEvent, const edm::EventSetup& iSetup);
   void POGtaus(const edm::Event& iEvent, const edm::EventSetup& iSetup);
 
   void clearVariables() ;
@@ -90,7 +94,8 @@ class MakeTauTree : public edm::EDAnalyzer {
   // ----------member data ---------------------------
 
   // -- config info
-  edm::InputTag pogtauSrc_,jetSrc_, metSrc_,electronSrc_,muonSrc_,tauSrc_,genParticleSrc_;
+  edm::InputTag pogtauSrc_,jetSrc_, metSrc_,electronSrc_,muonSrc_,tauSrc_,genParticleSrc_,vtxSrc_;
+  bool storeAllJetInfo_;
 
   double matchradius_;
 
@@ -98,10 +103,12 @@ class MakeTauTree : public edm::EDAnalyzer {
 
   TTree* tree_;
 
-  //tree variables
+  //jet counters
+  int njets70;
   int njets50;
   int njets30;
-  int nbjets30;
+  int nCSVMbjets30;
+  int nCSVTbjets30;
 
   //store info on all reco jets
   int njets10;
@@ -116,6 +123,7 @@ class MakeTauTree : public edm::EDAnalyzer {
   bool jetMatchGenTau[100];
   float jetTauDr[100]; //to study matching
 
+  //event variables
   float MET;
   float METphi;
   int nVetoMuons;
@@ -123,17 +131,31 @@ class MakeTauTree : public edm::EDAnalyzer {
   int nVetoTaus;
   int nIsolatedTracks;
 
+  float maxPtIsoTrack_Iso;
+  float maxPtIsoTrack_Pt;
+  float minIsoIsoTrack_Iso;
+  float minIsoIsoTrack_Pt;
+
+
+
   int nLooseTaus15;
   int nVLooseTaus15;
   int nMediumTaus15;
   int nLooseTaus20;
   int nVLooseTaus20;
   int nMediumTaus20;
-  //TODO
-  //  float DeltaPhiMetJet1;
-  //  float DeltaPhiMetJet2;
-  //  float DeltaPhiMetJet3;
+  float DeltaPhiMetJet1;
+  float DeltaPhiMetJet2;
+  float DeltaPhiMetJet3;
+  float minDeltaPhiMetBM;
+  float minDeltaPhiMetBT;
   int nGoodPV;
+
+  int lowChmJetChm;
+  float lowChmJetLrm;
+  float lowChmJetCSV;
+  float lowChmJetMT;
+  float lowChmJetPt;
 
   //some variables for DeltaPhi (b, MET)
   //info for top decays and tau decays
@@ -178,6 +200,8 @@ MakeTauTree::MakeTauTree(const edm::ParameterSet& iConfig) :
   muonSrc_(iConfig.getParameter<edm::InputTag>("MuonVetoSource")),
   tauSrc_(iConfig.getParameter<edm::InputTag>("TauVetoSource")), //these are jets to be used in the indirect tau veto
   genParticleSrc_(iConfig.getParameter<edm::InputTag>("GenParticleSource")),
+  vtxSrc_(iConfig.getParameter<edm::InputTag>("VertexSource")),
+  storeAllJetInfo_(iConfig.getParameter<bool>("StoreAllJetInfo")),
   matchradius_(0.3),
   tree_(0)
 {
@@ -185,7 +209,7 @@ MakeTauTree::MakeTauTree(const edm::ParameterSet& iConfig) :
   edm::Service<TFileService> fs;
   //branch creation for tree done in beginJob instead of here
   tree_=  fs->make<TTree>("reducedTree","reducedTree");
-  //  tree_=new TTree("reducedTree","reducedTree");
+
 }
 
 
@@ -233,6 +257,12 @@ void MakeTauTree::clearVariables() {
       jetPassTauId[i]=false;
       jetMatchGenTau[i]=false;
    }
+   
+   DeltaPhiMetJet1=99;
+   DeltaPhiMetJet2=99;
+   DeltaPhiMetJet3=99;
+   minDeltaPhiMetBM=99; //set higher than physical range
+   minDeltaPhiMetBT=99; //set higher than physical range
 
 }
 
@@ -247,7 +277,7 @@ MakeTauTree::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 
   //order of the pieces here is fragile.
   //MET must come first, because other calculations depend on it
-  edm::Handle<edm::View<pat::MET> > mets;
+  edm::Handle<edm::View<reco::MET> > mets;
   iEvent.getByLabel(metSrc_, mets);
   MET = mets->front().et();
   METphi = mets->front().phi();
@@ -258,12 +288,22 @@ MakeTauTree::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
   //do gen tau <--> reco jet matching //must come after fillMCInfo()
   doTauJetMatching(iEvent,iSetup);
 
+  njets70=0;
   njets50=0;
   njets30=0;
-  nbjets30=0;
+  nCSVMbjets30=0;
+  nCSVTbjets30=0;
   njets10=0;
 
-  edm::Handle<edm::View<pat::Jet> > jets;
+  int njetsAllEta=0;
+
+  lowChmJetChm =1000; //initialize big because we're looking for the min value
+  lowChmJetLrm = -1;
+  lowChmJetCSV = -1;
+  lowChmJetMT = -1;
+  lowChmJetPt =-1;
+
+  edm::Handle<edm::View<pat::Jet> > jets; //jet collection had better be sorted by pT!
   iEvent.getByLabel(jetSrc_,jets);
   for (edm::View<pat::Jet>::const_iterator jet = jets->begin(); jet != jets->end(); ++jet) {
 
@@ -277,12 +317,13 @@ MakeTauTree::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
       jetCSV[njets10]=jet->bDiscriminator("combinedSecondaryVertexBJetTags");
       jetMT[njets10] =  sqrt(2*(jet->pt()*MET)*(1-cos( fabs(reco::deltaPhi(jet->phi(),METphi)) )));
 
-      pat::Jet * thisjet = new pat::Jet(*jet); //just a trick to get the right data format
-      jetChm[njets10] = getCHMdr(thisjet,0.3,1);
-      jetLrm[njets10] = getLRM(thisjet);
-      std::vector<pat::Jet*> jetv; jetv.push_back(thisjet);
+      //      pat::Jet * thisjet = new pat::Jet(*jet); //just a trick to get the right data format
+      jetChm[njets10] = getCHMdr(*jet,0.3,1);
+      jetLrm[njets10] = getLRM(*jet);
+      //      std::vector<pat::Jet*> jetv; jetv.push_back(thisjet);
+      std::vector<pat::Jet> jetv; jetv.push_back(*jet);
       jetPassTauId[njets10] = tauVeto(jetv,MET,METphi) && (jet->pt()>15) && (jetCSV[njets10]<=0.898) && fabs(jet->eta())<2.4;
-      delete thisjet;
+      //      delete thisjet;
 
       //finally, does the jet match a gen tau?
       double dr1 = reco::deltaR( jet->eta(), jet->phi(), tauGenEta[0],tauGenPhi[0]);
@@ -292,21 +333,57 @@ MakeTauTree::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
       jetMatchGenTau[njets10] = match1 || match2;
 
       jetTauDr[njets10] = (dr1<dr2) ? dr1 : dr2;
-
+      
+      //now look for the 'lowest chm' jet, with pT>15 and |eta|<2.4
+      //this is just copying jet-by-jet info into an event-by-event quantity for convenience
+      if (jet->pt() >=15 && fabs(jet->eta())<2.4) {
+	if ( jetChm[njets10] < lowChmJetChm) {
+	  lowChmJetChm = jetChm[njets10];
+	  lowChmJetLrm = jetLrm[njets10];
+	  lowChmJetCSV = jetCSV[njets10];
+	  lowChmJetMT =  jetMT[njets10];
+	  lowChmJetPt =  jetPt[njets10];
+	}
+      }
+      
+      
       njets10++;
+    }
+
+
+    //use all jets within |eta|<5. 2011 note says 4.7; that might have been an artifact of JEC problems
+    //there is a nasty detail here about whether or not to apply jetID to these jets
+    //logic says no, but jet collection might say yes!
+    //moot if we veto any event that has a jet failing jet id (which is recommended in 2012)
+    if (jet->pt() >=30 && fabs(jet->eta())<5) {
+      ++njetsAllEta;
+      if (njetsAllEta==1) DeltaPhiMetJet1 = reco::deltaPhi(jet->phi(),METphi);
+      else if (njetsAllEta==2) DeltaPhiMetJet2 = reco::deltaPhi(jet->phi(),METphi);
+      else if (njetsAllEta==3) DeltaPhiMetJet3 = reco::deltaPhi(jet->phi(),METphi);
     }
 
     if ( fabs(jet->eta()) < 2.4 ) {
       float pt = jet->pt();
       if ( pt >= 30) {
 	++njets30;
-	if (jet->bDiscriminator("combinedSecondaryVertexBJetTags") > 0.898) ++nbjets30;
+	if (jet->bDiscriminator("combinedSecondaryVertexBJetTags") > 0.679) {
+	  ++nCSVMbjets30;
+	  if ( reco::deltaPhi(jet->phi(),METphi) < minDeltaPhiMetBM) minDeltaPhiMetBM=reco::deltaPhi(jet->phi(),METphi);
+	}
+	if (jet->bDiscriminator("combinedSecondaryVertexBJetTags") > 0.898) {
+	  ++nCSVTbjets30;
+	  if ( reco::deltaPhi(jet->phi(),METphi) < minDeltaPhiMetBT) minDeltaPhiMetBT=reco::deltaPhi(jet->phi(),METphi);
+	}
 	if (pt>=50) 	  ++njets50;
+	if (pt>=70) 	  ++njets70;
       }
     }
   }
 
+  //the first of these is the Lowette bare-bones implementation.
+  //the 2nd is the direct hooberman version that gives a bit more info.
   isolatedTrackVeto(iEvent,iSetup);
+  isolatedTrackDetails(iEvent,iSetup);
   POGtaus(iEvent,iSetup);
 
   edm::Handle<edm::View<reco::GsfElectron> > electrons;
@@ -322,7 +399,7 @@ MakeTauTree::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
   nVetoTaus = taus->size();
 
   edm::Handle<edm::View<reco::Vertex> > vertices;
-  iEvent.getByLabel("offlinePrimaryVertices", vertices); //this one those be customized
+  iEvent.getByLabel(vtxSrc_, vertices); //this one those be customized
   nGoodPV = vertices->size(); // now we know this is wrong. no quality cuts applied!
 
   //fill tree
@@ -609,10 +686,10 @@ void MakeTauTree::doTauJetMatching(const edm::Event& iEvent, const edm::EventSet
 	//MET and METphi should already be filled for this event (ntuple variables), but this is somewhat dangerous I suppose
 	taujetMT[ii] = sqrt(2*(jet->pt()*MET)*(1-cos( fabs(reco::deltaPhi(jet->phi(),METphi)) ))); //calculate transverse mass
 
-	pat::Jet * thisjet = new pat::Jet(*jet); //just a trick to get the right data format
-	taujetChm[ii] = getCHMdr(thisjet, 0.3, 1.); //calculate CHM
-	taujetLrm[ii] = getLRM(thisjet); //LRM
-	delete thisjet; //clean up!
+	//	pat::Jet * thisjet = new pat::Jet(*jet); //just a trick to get the right data format
+	taujetChm[ii] = getCHMdr(*jet, 0.3, 1.); //calculate CHM
+	taujetLrm[ii] = getLRM(*jet); //LRM
+	//delete thisjet; //clean up!
 
 	foundmatch[ii]=true;
       }
@@ -622,7 +699,23 @@ void MakeTauTree::doTauJetMatching(const edm::Event& iEvent, const edm::EventSet
 
 }
 
+//this calls steven's redo of the ben hooberman code
 void MakeTauTree::isolatedTrackVeto(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
+
+  edm::Handle<reco::PFCandidateCollection > isolatedcands;
+  iEvent.getByLabel("trackIsolationSelector",isolatedcands);
+
+  nIsolatedTracks =  isolatedcands->size();
+
+}
+
+//requires running Ben Hooberman's version of the code in the path
+void MakeTauTree::isolatedTrackDetails(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
+
+  maxPtIsoTrack_Iso=-1;
+  maxPtIsoTrack_Pt=-1;
+  minIsoIsoTrack_Iso=1e9;
+  minIsoIsoTrack_Pt=-1;
 
   edm::Handle<std::vector<float> > cand_dzpv;
   iEvent.getByLabel("trackIsolationMaker","pfcandsdzpv",cand_dzpv);
@@ -636,14 +729,28 @@ void MakeTauTree::isolatedTrackVeto(const edm::Event& iEvent, const edm::EventSe
   edm::Handle<std::vector<int> > cand_chg;
   iEvent.getByLabel("trackIsolationMaker","pfcandschg",cand_chg);
 
-  nIsolatedTracks = 0; // essential to reset to 0 for each event!
-
+  
   size_t n = cand_dzpv->size();
   for ( size_t ii = 0; ii<n; ii++) {
-
     //    std::cout<<ii<<"\t"<<cand_dzpv->at(ii)<<" "<<cand_pt->at(ii)<<" "<<cand_trkiso->at(ii)<<" "<<cand_chg->at(ii)<<std::endl;
-    if ( cand_chg->at(ii) != 0 ) { //charged
-      if ( (cand_pt->at(ii) > 10) && (fabs(cand_dzpv->at(ii)) < 0.05) && (cand_trkiso->at(ii)/cand_pt->at(ii) <0.1) ) nIsolatedTracks++;
+    if ( cand_chg->at(ii) != 0  && (fabs(cand_dzpv->at(ii)) < 0.05)) { //charged and compatible with PV
+      //      if ( (cand_pt->at(ii) > 10) && (cand_trkiso->at(ii)/cand_pt->at(ii) <0.1) ) nIsolatedTracks++;
+
+      if (cand_pt->at(ii) > 10) { //look for track above pT threshold that is most isolated
+	if (cand_trkiso->at(ii)/cand_pt->at(ii) < minIsoIsoTrack_Iso) {
+	  minIsoIsoTrack_Iso = cand_trkiso->at(ii)/cand_pt->at(ii);
+	  minIsoIsoTrack_Pt = cand_pt->at(ii);
+	}
+      }
+
+      //now look for track under iso threshold that has highest pT
+      if (cand_trkiso->at(ii)/cand_pt->at(ii) <0.10) { 
+	if (cand_pt->at(ii) >maxPtIsoTrack_Pt) {
+	  maxPtIsoTrack_Iso = cand_trkiso->at(ii)/cand_pt->at(ii);
+	  maxPtIsoTrack_Pt = cand_pt->at(ii);
+	}
+      }
+
     }
   }
 
@@ -685,12 +792,27 @@ MakeTauTree::beginJob()
 {
   tree_->Branch("nGoodPV",&nGoodPV,"nGoodPV/I");
 
+  tree_->Branch("njets70",&njets70,"njets70/I");
   tree_->Branch("njets50",&njets50,"njets50/I");
   tree_->Branch("njets30",&njets30,"njets30/I");
-  tree_->Branch("nbjets30",&nbjets30,"nbjets30/I");
+  tree_->Branch("nCSVMbjets30",&nCSVMbjets30,"nCSVMbjets30/I");
+  tree_->Branch("nCSVTbjets30",&nCSVTbjets30,"nCSVTbjets30/I");
 
   tree_->Branch("MET",&MET,"MET/F");
   tree_->Branch("METphi",&METphi,"METphi/F");
+
+  tree_->Branch("DeltaPhiMetJet1",&DeltaPhiMetJet1,"DeltaPhiMetJet1/F");
+  tree_->Branch("DeltaPhiMetJet2",&DeltaPhiMetJet2,"DeltaPhiMetJet2/F");
+  tree_->Branch("DeltaPhiMetJet3",&DeltaPhiMetJet3,"DeltaPhiMetJet3/F");
+
+  tree_->Branch("minDeltaPhiMetBM",&minDeltaPhiMetBM,"minDeltaPhiMetBM/F");
+  tree_->Branch("minDeltaPhiMetBT",&minDeltaPhiMetBT,"minDeltaPhiMetBT/F");
+
+  tree_->Branch("lowChmJetChm",&lowChmJetChm,"lowChmJetChm/I");
+  tree_->Branch("lowChmJetLrm",&lowChmJetLrm,"lowChmJetLrm/F");
+  tree_->Branch("lowChmJetCSV",&lowChmJetCSV,"lowChmJetCSV/F");
+  tree_->Branch("lowChmJetMT",&lowChmJetMT,"lowChmJetMT/F");
+  tree_->Branch("lowChmJetPt",&lowChmJetPt,"lowChmJetPt/F");
 
   tree_->Branch("nVetoElectrons",&nVetoElectrons,"nVetoElectrons/I");
   tree_->Branch("nVetoMuons",&nVetoMuons,"nVetoMuons/I");
@@ -705,6 +827,12 @@ MakeTauTree::beginJob()
   tree_->Branch("nMediumTaus20",&nMediumTaus20,"nMediumTaus20/I");
 
   tree_->Branch("nIsolatedTracks",&nIsolatedTracks,"nIsolatedTracks/I");
+
+  tree_->Branch("maxPtIsoTrack_Iso",&maxPtIsoTrack_Iso,"maxPtIsoTrack_Iso/F");
+  tree_->Branch("maxPtIsoTrack_Pt",&maxPtIsoTrack_Pt,"maxPtIsoTrack_Pt/F");
+  tree_->Branch("minIsoIsoTrack_Iso",&minIsoIsoTrack_Iso,"minIsoIsoTrack_Iso/F");
+  tree_->Branch("minIsoIsoTrack_Pt",&minIsoIsoTrack_Pt,"minIsoIsoTrack_Pt/F");
+
 
   tree_->Branch("topDecayCode",&topDecayCode,"topDecayCode[2]/I");
   tree_->Branch("ttbarDecayCode",&ttbarDecayCode,"ttbarDecayCode/I");
@@ -723,17 +851,21 @@ MakeTauTree::beginJob()
   tree_->Branch("taujetChm",&taujetChm,"taujetChm[2]/I");
   tree_->Branch("taujetLrm",&taujetLrm,"taujetLrm[2]/F");
 
-  tree_->Branch("njets10",&njets10,"njets10/I");
-  tree_->Branch("jetPt",jetPt,"jetPt[njets10]/F");
-  tree_->Branch("jetEta",jetEta,"jetEta[njets10]/F");
-  tree_->Branch("jetPhi",jetPhi,"jetPhi[njets10]/F");
-  tree_->Branch("jetCSV",jetCSV,"jetCSV[njets10]/F");
-  tree_->Branch("jetMT",jetMT,"jetMT[njets10]/F");
-  tree_->Branch("jetChm",jetChm,"jetChm[njets10]/I");
-  tree_->Branch("jetLrm",jetLrm,"jetLrm[njets10]/F");
-  tree_->Branch("jetPassTauId",jetPassTauId,"jetPassTauId[njets10]/O");
-  tree_->Branch("jetMatchGenTau",jetMatchGenTau,"jetMatchGenTau[njets10]/O");
-  tree_->Branch("jetTauDr",jetTauDr,"jetTauDr[njets10]/F");//to study matching
+  //for reasons of code simplicity, this is the *only* place that we turn off part of the code with StoreAllJetInfo
+  if (storeAllJetInfo_) {
+    tree_->Branch("njets10",&njets10,"njets10/I");
+    tree_->Branch("jetPt",jetPt,"jetPt[njets10]/F");
+    tree_->Branch("jetEta",jetEta,"jetEta[njets10]/F");
+    tree_->Branch("jetPhi",jetPhi,"jetPhi[njets10]/F");
+    tree_->Branch("jetCSV",jetCSV,"jetCSV[njets10]/F");
+    tree_->Branch("jetMT",jetMT,"jetMT[njets10]/F");
+    tree_->Branch("jetChm",jetChm,"jetChm[njets10]/I");
+    tree_->Branch("jetLrm",jetLrm,"jetLrm[njets10]/F");
+    tree_->Branch("jetPassTauId",jetPassTauId,"jetPassTauId[njets10]/O");
+    tree_->Branch("jetMatchGenTau",jetMatchGenTau,"jetMatchGenTau[njets10]/O");
+    tree_->Branch("jetTauDr",jetTauDr,"jetTauDr[njets10]/F");//to study matching
+  }
+
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
