a7d91426823b5a03d124ad39120467376dbd71cc
Josh <joshua.thompson@cern.ch>
2014-10-23 17:36:40 +0200

    get rid of pujetid option (never used in actual analysis)

--- DelphesCode/scripts/SubmissionTemplate.sh
+++ DelphesCode/scripts/SubmissionTemplate.sh
@@ -2,7 +2,7 @@
 echo "~~~ Started"
 workdir=$PWD
 exedir=/afs/cern.ch/work/j/joshmt/private/Upgrade/Delphes
-outputdir=/eos/cms/store/user/joshmt/upgrade/v10-PUID/XXXOUTPATHXXX
+outputdir=/eos/cms/store/user/joshmt/upgrade/v10/XXXOUTPATHXXX
 outfilename=reducedTree.def.XXXSAMPLEIDXXX_IJOB_NJOB.root
 
 eoscmd=/afs/cern.ch/project/eos/installation/0.3.15/bin/eos.select
@@ -12,7 +12,7 @@ cd /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.09/x86_64-slc6-gcc47-opt/root
 source bin/thisroot.sh
 cd $exedir
 echo "FlatTree starting"
-./FlatTree "root://eoscms.cern.ch/XXXEOSPATHXXX/XXXSAMPLEIDXXX/*.root" -O ${workdir}/${outfilename} -N IJOB NJOB -pujetid
+./FlatTree "root://eoscms.cern.ch/XXXEOSPATHXXX/XXXSAMPLEIDXXX/*.root" -O ${workdir}/${outfilename} -N IJOB NJOB 
 ls -l ${workdir}/*.root
 ${eoscmd} mkdir -p $outputdir
 ${eoscmd} cp ${workdir}/${outfilename} ${outputdir}/
